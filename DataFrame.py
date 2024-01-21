aa5c6e67632e19d973e156807e93f039756a87ab
Yogendra Prasad <127005415+pryogendra@users.noreply.github.com>
2024-01-21 14:42:59 +0530

    Python
    Create a simple data frame and display it.

--- DataFrame.py
+++ DataFrame.py
@@ -3,4 +3,4 @@ import numpy as np
 import matplotlib.pyplot as plt
 df=pd.DataFrame({"A":[1,2,3,4,5,6,7,8,9],'B':[11,12,13,14,15,16,17,18,19],'C':['a','b','c','d','e','f','g','h','i']})
 print(df)
-df.describe()
+print(df.describe())
